da9f47db5c3efa7b812cc1d60a32dfe517a4141d
enriqueBC <enrique_bernal@ciencias.unam.mx>
2016-05-15 20:27:24 -0500

    Cambio minimo a inserts

--- inserts.sql
+++ inserts.sql
@@ -3,4 +3,4 @@ INSERT INTO usuario (nombre, correo, contrasena, calificacion, esAdmin)
 VALUES ('kike','kike@mail.com',md5('kike'),0,true);
 
 INSERT INTO usuario(nombre,correo,contrasena,calificacion, esadmin)
-VALUES ('usuario1', 'usuario@gmail.com', md5('12345'), 0, FALSE);
+VALUES ('usuario', 'usuario@gmail.com', md5('12345'), 0, FALSE);
