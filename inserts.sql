31b60c1bb277410833cbb94c71e4344865dbcbdf
enriqueBC <enrique_bernal@ciencias.unam.mx>
2016-05-30 01:04:24 -0500

    Correccion a inserts antiguos
    Corrección realizada a inserts pasados para concordar con la base de
    datos actual

--- inserts.sql
+++ inserts.sql
@@ -1,6 +1,6 @@
 -- Inserts para tener usuarios en la base de datos
-INSERT INTO usuario(nombre,correo,contrasena,calificacion, esadmin)
-VALUES ('usuario', 'usuario@gmail.com', md5('12345'), 0, FALSE);
+INSERT INTO usuario(nombre,correo,contrasena,calificacion, esadmin, habilitado, informacion, telefono)
+VALUES ('usuario', 'usuario@gmail.com', md5('12345'), 0, FALSE, TRUE, 'Yo soy el primer usuario c:', '666');
 
-INSERT INTO usuario (nombre, correo, contrasena, calificacion, esAdmin)
-VALUES ('kike','kike@mail.com',md5('kike'),0,true);
+INSERT INTO usuario (nombre, correo, contrasena, calificacion, esAdmin, habilitado, informacion, telefono)
+VALUES ('kike', 'kike@mail.com', md5('kike'), 0, TRUE, TRUE, 'Administrador', '56194403');
